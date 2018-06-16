6b5e02c8127c68b9d7c06e333ee78a22dd3af58b
JJbas <jjbastien78@gmail.com>
2018-06-16 08:02:16 -0500

    i added the html

---
+++ assets/javascript/main.js
@@ -0,0 +1,25 @@
+
+  
+$(document).ready(function(){
+
+
+  
+
+  $("#trainBtn").on("click", function(){
+      var Name = $("#trainName").val().trim();
+      var destination  = $("#destination").val().trim();
+      var firstTrainTime = ("#firstTrainTime").val().trim() 
+      var frequency = $("#frequency").val().trim();
+      console.log(Name)
+      console.log(destination)
+      console.log(firstTrainTime)
+      console.log(frequency)
+     
+
+      
+    
+
+      
+  })
+})
+
