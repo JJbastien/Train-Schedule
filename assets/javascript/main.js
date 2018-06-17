c759ec621c798c21f1b47fb92c3be40cf96370ff
JJbas <jjbastien78@gmail.com>
2018-06-16 22:52:48 -0500

    submission

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
       var name = $("#trainName").val().trim();
       var destination  = $("#destination").val().trim();
       //convert firstrain time into military time
-      var firstTrain = $("#firstTrain").val().trim()
+      var firstTrain = moment($("#firstTrain").val().trim(), "HH:mm").subtract(1, "year").format("x")
       var frequency = $("#frequency").val().trim();
       var trainData = firebase.database()
       
@@ -39,16 +39,16 @@ $(document).ready(function(){
       trainData.ref().push(newTrain)
      })
      //this is the function to retrieve data from firebase and to display on the screen
-     trainData.ref().on("child_added",function(snapshop){
+        trainData.ref().on("child_added",function(snapshop){
          var name = snapshop.val().name
          var destination = snapshop.val().destination
          var firstTrain = snapshop.val().firstTrain
          var frequency = snapshop.val().frequency
-         //calculation for wait time . using the diff method
-         var waitTime = moment().diff(moment(firstTrain),"minutes")%frequency
-        
-         var minutes = frequency - waitTime
-         var newArrival = moment().add(minutes).format("hh:MM");
+          var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency
+          var minutes = frequency - remainder
+          console.log(moment())
+          var newArrival = moment().add(minutes).format("HH:mm A")
+      
          $("#trainTable tbody").append("<tr><td>" + name +"</td><td>"+ destination + "</td><td>"+ frequency+ "</td><td>"+ newArrival + "</td><td>" + minutes + "</td></tr>")
          
          
